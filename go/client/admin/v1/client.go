4950c62e8259db0233e9c20ccf3efec925a49441
Stefan Majer <stefan.majer@f-i-ts.de>
2023-04-27 14:18:33 +0200

    Fix client

--- go/client/admin/v1/client.go
+++ go/client/admin/v1/client.go
@@ -15,6 +15,7 @@ type Client interface {
 	Tenant() adminv1connect.TenantServiceClient
 	Payment() adminv1connect.PaymentServiceClient
 	Storage() adminv1connect.StorageServiceClient
+	Cluster() adminv1connect.ClusterServiceClient
 }
 
 // admin is a client implementation of the api with grpc transport.
@@ -23,6 +24,7 @@ type admin struct {
 	tenantServiceClient  adminv1connect.TenantServiceClient
 	paymentServiceClient adminv1connect.PaymentServiceClient
 	storageServiceClient adminv1connect.StorageServiceClient
+	clusterServiceClient adminv1connect.ClusterServiceClient
 }
 
 func New(ctx context.Context, config client.DialConfig) Client {
@@ -43,6 +45,11 @@ func New(ctx context.Context, config client.DialConfig) Client {
 			config.BaseURL,
 			compress.WithAll(compress.LevelBalanced),
 		),
+		clusterServiceClient: adminv1connect.NewClusterServiceClient(
+			config.HttpClient(),
+			config.BaseURL,
+			compress.WithAll(compress.LevelBalanced),
+		),
 	}
 }
 
@@ -54,4 +61,7 @@ func (c admin) Payment() adminv1connect.PaymentServiceClient {
 }
 func (c admin) Storage() adminv1connect.StorageServiceClient {
 	return c.storageServiceClient
+}
+func (c admin) Cluster() adminv1connect.ClusterServiceClient {
+	return c.clusterServiceClient
 }
