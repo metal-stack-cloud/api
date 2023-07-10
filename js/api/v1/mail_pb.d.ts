8c3887de622159d5db43b16aa2850b760891ebbc
Pilz, Alexander <alexander.pilz@x-cellent.com>
2023-07-10 13:12:46 +0200

    move mail types to api

---
+++ js/api/v1/mail_pb.d.ts
@@ -0,0 +1,33 @@
+/**
+ * @generated from enum api.v1.MailType
+ */
+export declare enum MailType {
+    /**
+     * @generated from enum value: MAIL_TYPE_UNSPECIFIED = 0;
+     */
+    UNSPECIFIED = 0,
+    /**
+     * @generated from enum value: MAIL_TYPE_ADMITTED = 1;
+     */
+    ADMITTED = 1,
+    /**
+     * @generated from enum value: MAIL_TYPE_COUPON_PERCENTAGE_USED_WARNING = 2;
+     */
+    COUPON_PERCENTAGE_USED_WARNING = 2,
+    /**
+     * @generated from enum value: MAIL_TYPE_COUPON_COMPLETELY_USED_WARNING = 3;
+     */
+    COUPON_COMPLETELY_USED_WARNING = 3,
+    /**
+     * @generated from enum value: MAIL_TYPE_PAYMENT_UPDATED = 4;
+     */
+    PAYMENT_UPDATED = 4,
+    /**
+     * @generated from enum value: MAIL_TYPE_ADMISSION_REQUESTED = 5;
+     */
+    ADMISSION_REQUESTED = 5,
+    /**
+     * @generated from enum value: MAIL_TYPE_WELCOME = 6;
+     */
+    WELCOME = 6
+}
