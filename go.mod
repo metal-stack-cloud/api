17ffb0819d945ea5f81076212153d7bba836d6e9
Stefan Majer <stefan.majer@f-i-ts.de>
2023-11-14 09:14:35 +0100

    Clusters can have a api-server-acl specified

--- go.mod
+++ go.mod
@@ -9,9 +9,9 @@ require (
 	github.com/envoyproxy/protoc-gen-validate v1.0.2
 	github.com/klauspost/connect-compress/v2 v2.0.0
 	github.com/stretchr/testify v1.8.4
-	golang.org/x/exp v0.0.0-20231006140011-7918f672742d
+	golang.org/x/exp v0.0.0-20231110203233-9a3e6036ecaa
 	google.golang.org/protobuf v1.31.0
-)	
+)
 
 require (
 	github.com/Masterminds/goutils v1.1.1 // indirect
@@ -27,7 +27,7 @@ require (
 	github.com/shopspring/decimal v1.3.1 // indirect
 	github.com/spf13/cast v1.5.1 // indirect
 	github.com/stretchr/objx v0.5.1 // indirect
-	golang.org/x/crypto v0.14.0 // indirect
+	golang.org/x/crypto v0.15.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- go/api/v1/cluster.pb.validate.go
+++ go/api/v1/cluster.pb.validate.go
@@ -334,6 +334,35 @@ func (m *Cluster) validate(all bool) error {
 		}
 	}
 
+	if all {
+		switch v := interface{}(m.GetApiServerAcl()).(type) {
+		case interface{ ValidateAll() error }:
+			if err := v.ValidateAll(); err != nil {
+				errors = append(errors, ClusterValidationError{
+					field:  "ApiServerAcl",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		case interface{ Validate() error }:
+			if err := v.Validate(); err != nil {
+				errors = append(errors, ClusterValidationError{
+					field:  "ApiServerAcl",
+					reason: "embedded message failed validation",
+					cause:  err,
+				})
+			}
+		}
+	} else if v, ok := interface{}(m.GetApiServerAcl()).(interface{ Validate() error }); ok {
+		if err := v.Validate(); err != nil {
+			return ClusterValidationError{
+				field:  "ApiServerAcl",
+				reason: "embedded message failed validation",
+				cause:  err,
+			}
+		}
+	}
+
 	if m.Purpose != nil {
 		// no validation rules for Purpose
 	}
@@ -1176,6 +1205,121 @@ var _ interface {
 	ErrorName() string
 } = WorkerUpdateValidationError{}
 
+// Validate checks the field values on APIServerACL with the rules defined in
+// the proto definition for this message. If any rules are violated, the first
+// error encountered is returned, or nil if there are no violations.
+func (m *APIServerACL) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on APIServerACL with the rules defined
+// in the proto definition for this message. If any rules are violated, the
+// result is a list of violation errors wrapped in APIServerACLMultiError, or
+// nil if none found.
+func (m *APIServerACL) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *APIServerACL) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	for idx, item := range m.GetSourceAddresses() {
+		_, _ = idx, item
+
+		if ip := net.ParseIP(item); ip == nil {
+			err := APIServerACLValidationError{
+				field:  fmt.Sprintf("SourceAddresses[%v]", idx),
+				reason: "value must be a valid IP address",
+			}
+			if !all {
+				return err
+			}
+			errors = append(errors, err)
+		}
+
+	}
+
+	if len(errors) > 0 {
+		return APIServerACLMultiError(errors)
+	}
+
+	return nil
+}
+
+// APIServerACLMultiError is an error wrapping multiple validation errors
+// returned by APIServerACL.ValidateAll() if the designated constraints aren't met.
+type APIServerACLMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m APIServerACLMultiError) Error() string {
+	var msgs []string
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m APIServerACLMultiError) AllErrors() []error { return m }
+
+// APIServerACLValidationError is the validation error returned by
+// APIServerACL.Validate if the designated constraints aren't met.
+type APIServerACLValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e APIServerACLValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e APIServerACLValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e APIServerACLValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e APIServerACLValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e APIServerACLValidationError) ErrorName() string { return "APIServerACLValidationError" }
+
+// Error satisfies the builtin error interface
+func (e APIServerACLValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sAPIServerACL.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = APIServerACLValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = APIServerACLValidationError{}
+
 // Validate checks the field values on ClusterServiceGetRequest with the rules
 // defined in the proto definition for this message. If any rules are
 // violated, the first error encountered is returned, or nil if there are no violations.
@@ -1989,6 +2133,39 @@ func (m *ClusterServiceUpdateRequest) validate(all bool) error {
 			if err := v.Validate(); err != nil {
 				return ClusterServiceUpdateRequestValidationError{
 					field:  "Maintenance",
+					reason: "embedded message failed validation",
+					cause:  err,
+				}
+			}
+		}
+
+	}
+
+	if m.ApiServerAcl != nil {
+
+		if all {
+			switch v := interface{}(m.GetApiServerAcl()).(type) {
+			case interface{ ValidateAll() error }:
+				if err := v.ValidateAll(); err != nil {
+					errors = append(errors, ClusterServiceUpdateRequestValidationError{
+						field:  "ApiServerAcl",
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			case interface{ Validate() error }:
+				if err := v.Validate(); err != nil {
+					errors = append(errors, ClusterServiceUpdateRequestValidationError{
+						field:  "ApiServerAcl",
+						reason: "embedded message failed validation",
+						cause:  err,
+					})
+				}
+			}
+		} else if v, ok := interface{}(m.GetApiServerAcl()).(interface{ Validate() error }); ok {
+			if err := v.Validate(); err != nil {
+				return ClusterServiceUpdateRequestValidationError{
+					field:  "ApiServerAcl",
 					reason: "embedded message failed validation",
 					cause:  err,
 				}
