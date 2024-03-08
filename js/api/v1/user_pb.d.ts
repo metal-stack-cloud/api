851c225cfbae7a9fc39fd0035c1390ba4472e597
Stefan Majer <stefan.majer@f-i-ts.de>
2024-03-08 12:53:29 +0100

    Keep user

---
+++ js/api/v1/user_pb.d.ts
@@ -0,0 +1,62 @@
+import type { BinaryReadOptions, FieldList, JsonReadOptions, JsonValue, PartialMessage, PlainMessage } from "@bufbuild/protobuf";
+import { Message, proto3 } from "@bufbuild/protobuf";
+import { OAuthProvider } from "./common_pb.js";
+import { Tenant } from "./tenant_pb.js";
+import { Project } from "./project_pb.js";
+/**
+ * User is a end user of the platform
+ *
+ * @generated from message api.v1.User
+ */
+export declare class User extends Message<User> {
+    /**
+     * Login the login at the provider
+     *
+     * @generated from field: string login = 1;
+     */
+    login: string;
+    /**
+     * Name of the user
+     *
+     * @generated from field: string name = 2;
+     */
+    name: string;
+    /**
+     * Email of the user
+     *
+     * @generated from field: string email = 3;
+     */
+    email: string;
+    /**
+     * AvatarUrl of the user
+     *
+     * @generated from field: string avatar_url = 5;
+     */
+    avatarUrl: string;
+    /**
+     * OauthProvider of the user
+     *
+     * @generated from field: api.v1.OAuthProvider oauth_provider = 6;
+     */
+    oauthProvider: OAuthProvider;
+    /**
+     * Tenants the user belongs to
+     *
+     * @generated from field: repeated api.v1.Tenant tenants = 7;
+     */
+    tenants: Tenant[];
+    /**
+     * Projects the user belongs to
+     *
+     * @generated from field: repeated api.v1.Project projects = 8;
+     */
+    projects: Project[];
+    constructor(data?: PartialMessage<User>);
+    static readonly runtime: typeof proto3;
+    static readonly typeName = "api.v1.User";
+    static readonly fields: FieldList;
+    static fromBinary(bytes: Uint8Array, options?: Partial<BinaryReadOptions>): User;
+    static fromJson(jsonValue: JsonValue, options?: Partial<JsonReadOptions>): User;
+    static fromJsonString(jsonString: string, options?: Partial<JsonReadOptions>): User;
+    static equals(a: User | PlainMessage<User> | undefined, b: User | PlainMessage<User> | undefined): boolean;
+}
