3ab8ab8f63186bcafb3175712c0bf581eb680d6c
Stefan Majer <stefan.majer@f-i-ts.de>
2025-08-01 11:47:32 +0200

    Simpler

--- examples/python/ip-list.py
+++ examples/python/ip-list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
-from connecpy.context import ClientContext
-from connecpy.exceptions import ConnecpyServerException
 import os
+import sys
 
-from api.v1 import ip_pb2, ip_connecpy
+from connecpy.exceptions import ConnecpyServerException
+
+from api.v1 import ip_pb2
 from client import client as apiclient
 
 
@@ -13,38 +14,15 @@ baseurl = os.environ['METAL_APISERVER_URL']
 token = os.environ['API_TOKEN']
 project = os.environ['PROJECT_ID']
 
-def main():
-    # Usage Sample without driver
-    print("ips with direct client")
-    with ip_connecpy.IPServiceClient(baseurl, timeout=timeout_s) as client:
-        try:
-            response = client.List(
-                ctx=ClientContext(),
-                request=ip_pb2.IPServiceListRequest(project=project),
-                headers={
-                    "Authorization": "Bearer " + token,
-                }
-            )
-            for ip in response.ips:
-                print(ip.ip, ip.name, ip.project, ip.network)
-        except ConnecpyServerException as e:
-            print(e.code, e.message, e.to_dict())
+client = apiclient.Client(baseurl=baseurl, token=token, timeout=timeout_s)
+
+try:
+    resp = client.apiv2().ip().List(request=ip_pb2.IPServiceListRequest(
+        project=project))
+except ConnecpyServerException as e:
+    print(e.code, e.message, e.to_dict())
+    sys.exit(1)
 
 
-    # Usage Sample with driver
-    driver = apiclient.Driver(baseurl=baseurl, token=token, timeout=timeout_s)
-    
-    print()
-    print("ips with driver")
-    with driver.api().ip() as client:
-        try:
-            response = client.List(
-                request=ip_pb2.IPServiceListRequest(project=project),
-            )
-            for ip in response.ips:
-                print(ip.ip, ip.name, ip.project, ip.network)
-        except ConnecpyServerException as e:
-            print(e.code, e.message, e.to_dict())
-
-if __name__ == "__main__":
-    main()
+for ip in resp.ips:
+    print(ip.ip, ip.name, ip.project, ip.network)
